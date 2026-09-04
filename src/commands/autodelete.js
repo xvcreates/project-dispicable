0d5635ba6d1b1f82f6113f7f68988ad345ca58c1
xvcreates <xvcreates@users.noreply.github.com>
2026-09-05 00:18:25 +0100

    feat: replace deletefuture with channel based autodelete

---
+++ src/commands/autodelete.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
+const db = require('../services/database');
+const { memberHasCmds } = require('../utils/permissionUtils');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('autodelete')
+    .setDescription('Delete user messages in a selected channel; bot messages are kept.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addChannelOption(option => option
+      .setName('channel')
+      .setDescription('Channel where user messages should be deleted')
+      .setRequired(true)
+      .addChannelTypes(ChannelType.GuildText)),
+
+  async execute(interaction) {
+    if (!(await memberHasCmds(interaction.member))) {
+      return interaction.reply({ content: 'You need the cmds role to use this command.', ephemeral: true });
+    }
+
+    const channel = interaction.options.getChannel('channel');
+    const botMember = interaction.guild.members.me;
+    if (!botMember || !channel.permissionsFor(botMember).has(PermissionFlagsBits.ManageMessages)) {
+      return interaction.reply({ content: `I need Manage Messages permission in ${channel}.`, ephemeral: true });
+    }
+
+    await db.enableDeleteFuture(interaction.guild.id, channel.id);
+    await interaction.reply({ content: `Auto-delete enabled in ${channel}. Bot messages will not be deleted.`, ephemeral: true });
+  }
+};
---
+++ src/commands/cancelautodelete.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
+const db = require('../services/database');
+const { memberHasCmds } = require('../utils/permissionUtils');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('cancelautodelete')
+    .setDescription('Stop auto-deleting user messages in a selected channel.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+    .addChannelOption(option => option
+      .setName('channel')
+      .setDescription('Channel where auto-delete should be disabled')
+      .setRequired(true)
+      .addChannelTypes(ChannelType.GuildText)),
+
+  async execute(interaction) {
+    if (!(await memberHasCmds(interaction.member))) {
+      return interaction.reply({ content: 'You need the cmds role to use this command.', ephemeral: true });
+    }
+
+    const channel = interaction.options.getChannel('channel');
+    await db.disableDeleteFuture(interaction.guild.id, channel.id);
+    await interaction.reply({ content: `Auto-delete disabled in ${channel}.`, ephemeral: true });
+  }
+};
